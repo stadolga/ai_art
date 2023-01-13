d5472337f8c76da87595709e5c377435dd84f190
Kaartokallio Ohto <ohto.kaartokallio@aalto.fi>
2023-01-14 01:58:49 +0200

    disabled submit button when processing

--- ai_draw_frontend/src/components/CanvasToAIButton.js
+++ ai_draw_frontend/src/components/CanvasToAIButton.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useCanvas } from '../CanvasContext';
+import { useSelector } from 'react-redux';
 
 export function CanvasToAIButton() {
-  const { CanvasToAI } = useCanvas();
+    const { CanvasToAI } = useCanvas();
+    const aiText = useSelector(state => state.response)
+    const loadingMessages = useSelector(state => state.error)
+    
+    const bool = aiText === "" || (loadingMessages.includes("Creating the image...") || loadingMessages.includes("Analyzing..."))
 
-  return <button className="submitButton" onClick={CanvasToAI}>Submit</button>;
+    return <button className={`submitButton ${bool ? "disabled-button" : ""}`} disabled={bool} onClick={CanvasToAI}>Submit</button>;
 }
